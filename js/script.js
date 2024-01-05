d8c2c3da810e25f126d97cc875e145814d96654c
zalish-shawon <zalish.shawon@gmail.com>
2024-01-06 03:52:25 +0600

    added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+let swiper = new Swiper(".swiper", {
+    slidesPerView: 4,
+    spaceBetween: 0,
+    centeredSlides: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
